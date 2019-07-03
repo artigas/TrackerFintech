a4924b2d8714e9a23315e7a19975a2d7bd34ea7f
artigas <martigas@mail.com>
2019-07-03 13:22:31 -0300

    Startup: AddMvc injection

--- TrackerFintech/Startup.cs
+++ TrackerFintech/Startup.cs
@@ -26,6 +26,8 @@ namespace TrackerFintech
             {
                 DeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
             });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
